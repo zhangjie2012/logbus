96a7255358f8a2bf4347576b5ee3609e382e5a05
JerryZhang <me@zhangjiee.com>
2020-10-20 21:41:48 +0800

    remove cbl-go

--- go.mod
+++ go.mod
@@ -3,12 +3,12 @@ module github.com/zhangjie2012/logbus
 go 1.14
 
 require (
+	github.com/go-redis/redis/v7 v7.4.0
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/prometheus/client_golang v1.8.0
 	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.6.1
 	github.com/vmihailenco/msgpack v4.0.4+incompatible
-	github.com/zhangjie2012/cbl-go v0.2.3
 	go.mongodb.org/mongo-driver v1.4.2
 	golang.org/x/sys v0.0.0-20201017003518-b09fb700fbb7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
--- input_redis.go
+++ input_redis.go
@@ -5,20 +5,26 @@ import (
 	"fmt"
 	"time"
 
+	redis "github.com/go-redis/redis/v7"
 	"github.com/vmihailenco/msgpack"
-	"github.com/zhangjie2012/cbl-go/cache"
 )
 
 // RedisListInput data from redis LIST
 type RedisListInput struct {
+	client  *redis.Client
 	listKey string
 }
 
 func NewRedisListInput(app string, addr string, password string, db int, key string) (Input, error) {
-	if err := cache.InitCache(app, addr, password, db); err != nil {
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
+	if _, err := client.Ping().Result(); err != nil {
 		return nil, err
 	}
-	return &RedisListInput{listKey: key}, nil
+	return &RedisListInput{client: client, listKey: key}, nil
 }
 
 func (in *RedisListInput) Read(ctx context.Context) (*StdLog, error) {
@@ -28,14 +34,16 @@ Loop:
 	case <-ctx.Done():
 		return nil, NoInputData
 	default:
-		bs, err := cache.MQBlockPop(in.listKey, 1*time.Second)
-		if err != nil {
-			if err == cache.NotExist {
-				goto Loop
-			}
+		result, err := in.client.BLPop(1*time.Second, in.listKey).Result()
+		if err != nil && err != redis.Nil {
 			return nil, fmt.Errorf("read data error: %s", err)
 		}
+		if err == redis.Nil {
+			goto Loop
+		}
 
+		// result[0] is key name
+		bs := []byte(result[1])
 		l := StdLog{}
 		if err := msgpack.Unmarshal(bs, &l); err != nil {
 			return nil, fmt.Errorf("marshal log error: %s", err)
@@ -45,6 +53,5 @@ Loop:
 }
 
 func (in *RedisListInput) Close() error {
-	cache.CloseCache()
-	return nil
+	return in.client.Close()
 }
